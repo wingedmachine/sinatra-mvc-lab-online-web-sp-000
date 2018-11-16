2ab82d65bdc40580cae5757bd3939685ff2705a7
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-16 00:38:06 +0000

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,16 @@
+class PigLatinizer
+  def piglatinize(phrase)
+    phrase.split(' ').map do |word|
+      piglatinize_word(word)
+    end.join(' ')
+  end
+
+  def piglatinize_word(word)
+    first_vowel_index = word.index(/[aeiouAEIOU]/)
+    if first_vowel_index == 0
+      "#{word}way"
+    else
+      "#{word[(first_vowel_index..word.length)]}#{word[0..first_vowel_index-1]}ay"
+    end
+  end
+end
