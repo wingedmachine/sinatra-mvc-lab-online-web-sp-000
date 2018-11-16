601e0bcb6570c72c32da101c40e0ec25c3d0f706
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-16 00:24:00 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,2 +1,10 @@
 class PigLatinizer
+  def piglatinize(phrase)
+    phrase.split(' ').map do |word|
+      piglatinize_word(word)
+    end.join(' ')
+  end
+
+  def piglatinize_word(word)
+  end
 end
