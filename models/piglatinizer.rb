aabff02b3858d4dda8151bfc54483550bf52f865
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-16 00:30:16 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -7,6 +7,10 @@ class PigLatinizer
 
   def piglatinize_word(word)
     first_vowel_index = word.index(/[aeiouAEIOU]/)
-    "#{word[(first_vowel_index..word.length)]}#{word[0..first_vowel_index-1]}ay"
+    if first_vowel_index == 0
+      "#{word}ay"
+    else
+      "#{word[(first_vowel_index..word.length)]}#{word[0..first_vowel_index-1]}ay"
+    end
   end
 end
